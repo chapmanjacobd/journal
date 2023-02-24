013e906ccbe5f228c833335facd8c03dd2d44acc
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-02-23 23:34:32 -0600

    cloud_batch.sh: Added 4 lines

---
+++ snippets/cloud_batch.sh
@@ -0,0 +1,8 @@
+docker exec -it $(docker container ls -l | tail -1 | cut -f1 -d' ') /bin/bash
+
+docker logs -f $(docker container ls -l | tail -1 | cut -f1 -d' ')
+
+toolbox apt install -y sysstat iftop ncdu lsof htop kitty-terminfo
+
+toolbox ncdu /media/root/mnt/stateful_partition/var/lib/docker/overlay2
+
