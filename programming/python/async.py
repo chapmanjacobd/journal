409ce1c6ebeded6ae40ef344841af2317d272e44
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2023-09-12 22:28:49 +0200

    async.py: Added 19 lines

---
+++ programming/python/async.py
@@ -0,0 +1,27 @@
+import functools
+
+
+def as_async(fn):
+    from concurrent.futures import ThreadPoolExecutor
+    import asyncio
+    pool = ThreadPoolExecutor()
+
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        future = pool.submit(fn, *args, **kwargs)
+        return asyncio.wrap_future(future)  # make it awaitable
+
+    return wrapper
+
+
+def as_sync(fn):
+    import asyncio
+
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        res = fn(*args, **kwargs)
+        if asyncio.iscoroutine(res):
+            return asyncio.get_event_loop().run_until_complete(res)
+        return res
+
+    return wrapper
