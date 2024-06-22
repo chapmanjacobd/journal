116e427a2c8f07337799893f4f5f49fcb89f5f4c
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-06-21 21:09:03 -0500

    singleton

---
+++ programming/python/singleton.py
@@ -0,0 +1,34 @@
+from threading import Lock, Thread
+
+
+class SingletonMeta(type):
+    _instances = {}
+
+    _lock: Lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:
+            if cls not in cls._instances:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Singleton(metaclass=SingletonMeta):
+    value: str = None
+
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+
+def test_singleton(value: str) -> None:
+    singleton = Singleton(value)
+    print(singleton.value)
+
+
+if __name__ == "__main__":
+
+    process1 = Thread(target=test_singleton, args=("FOO",))
+    process2 = Thread(target=test_singleton, args=("BAR",))
+    process1.start()
+    process2.start()
